f511a4459b4b711ee5374ae634f9f24021448839
rtavaresmorais <rtavaresmorais@gmail.com>
2025-03-26 15:16:56 -0300

    Segundo commit

--- app/Componentes/Saudacao.jsx
+++ app/Componentes/Saudacao.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Saudacao() {
   return (
     <div>
-      <h1>Olá, mundo!</h1>
+      <h1 >Olá, mundo ss!</h1>
       <p>Esse é um componente React simples para testar.</p>
     </div>
   );
