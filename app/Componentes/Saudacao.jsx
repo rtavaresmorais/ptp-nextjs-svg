38ff17a5b169fb87ebf7b0870571e82fa9f656e6
rtavaresmorais <rtavaresmorais@gmail.com>
2025-03-26 15:00:31 -0300

    Primeiro commit

---
+++ app/Componentes/Saudacao.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Saudacao() {
+  return (
+    <div>
+      <h1>Olá, mundo!</h1>
+      <p>Esse é um componente React simples para testar.</p>
+    </div>
+  );
+}
+
+export default Saudacao;
