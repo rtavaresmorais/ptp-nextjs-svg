6d48a97347c93b09602f8c703daa69ced074cd1b
rtavaresmorais <rtavaresmorais@gmail.com>
2025-03-26 15:44:10 -0300

    QUARTO COMMIT

--- app/Componentes/Saudacao.jsx
+++ app/Componentes/Saudacao.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Saudacao() {
   return (
     <div>
-      <h1 >Olá, mundo ultimo!</h1>
+      <h1 >Olá, mundo UFFA!</h1>
       <p>Esse é um componente React simples para testar.</p>
     </div>
   );
